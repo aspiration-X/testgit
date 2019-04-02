5fdb7f8e4b269dc989fcc0a4cb84d3ffb692787b
spir <1522258606@qq.com>
2019-04-02 23:43:07 +0800

    qqq

--- src/com/spir/HelloWorld.java
+++ src/com/spir/HelloWorld.java
@@ -6,6 +6,6 @@ package com.spir;
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("hello1");
+        System.out.println("hello12");
     }
 }
