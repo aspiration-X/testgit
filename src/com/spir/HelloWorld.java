a99dc8d9fbfe1c2bdfe46e4a570a36acf21467f0
spir <1522258606@qq.com>
2019-04-02 23:08:00 +0800

    first commit

---
+++ src/com/spir/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.spir;
+
+/**
+ * created by spir
+ * Date2019/4/2 Time 23:06
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
